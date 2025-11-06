3d37e89480d004661e572763a969465f5c41b71a
Manoj M V <manojmv047@gmail.com>
2025-11-07 01:22:25 +0530

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
+interface ImportMetaEnv {//hi
   readonly VITE_YOUTUBE_API_KEY: string | undefined;
   // add more environment variables here as needed
 }
